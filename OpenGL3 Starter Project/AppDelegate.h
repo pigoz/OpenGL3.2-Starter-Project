9c4f11c23d9e27c1962ff42fc9ff8275e4d9a60f
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-09-29 10:28:06 +0200

    initial project

---
+++ OpenGL3 Starter Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL3 Starter Project
+//
+//  Created by Stefano Pigozzi on 9/29/12.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ OpenGL3 Starter Project/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OpenGL3 Starter Project
+//
+//  Created by Stefano Pigozzi on 9/29/12.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}

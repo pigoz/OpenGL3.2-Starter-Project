d0b213e492ffcf9a51a44235331d30204a748405
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-09-29 21:16:59 +0200

    drive opengl rendering with displaylink

--- OpenGL3 Starter Project/AppDelegate.m
+++ OpenGL3 Starter Project/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  OpenGL3 Starter Project
-//
-//  Created by Stefano Pigozzi on 9/29/12.
-//
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
--- OpenGL3 Starter Project/views/OpenGLView.m
+++ OpenGL3 Starter Project/views/OpenGLView.m
@@ -3,7 +3,11 @@
 #define NSPIXFMT NSOpenGLPixelFormat
 #define NSCTX    NSOpenGLContext
 
-@implementation OpenGLView : NSOpenGLView
+#import <OpenGL/CGLMacro.h>
+
+@implementation OpenGLView {
+    CVDisplayLinkRef displayLink;
+}
 
 - (void)awakeFromNib
 {
@@ -31,11 +35,78 @@
     [context release];
 }
 
-- (void)drawRect:(NSRect)dirtyRect
+- (void)prepareOpenGL
 {
+    GLint vsync = 1;
+    CGLContextObj cglContext = [[self openGLContext] CGLContextObj];
+    CGLPixelFormatObj cglPixelFormat = [[self pixelFormat] CGLPixelFormatObj];
+
+    [[self openGLContext] setValues:&vsync forParameter:NSOpenGLCPSwapInterval];
+    CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+    CVDisplayLinkSetOutputCallback(displayLink, &displayLinkCallback, self);
+    CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext,
+                                                      cglPixelFormat);
+
+    CVDisplayLinkStart(displayLink);
+}
+
+- (CGLContextObj) willStartDrawing
+{
+    CGLContextObj cgl_ctx = [[self openGLContext] CGLContextObj];
+    CGLSetCurrentContext(cgl_ctx);
+	CGLLockContext(cgl_ctx);
+    return cgl_ctx;
+}
+
+- (void) didFinishDrawing: (CGLContextObj) cgl_ctx;
+{
+    CGLUnlockContext(cgl_ctx);
+}
+
+- (CVReturn)render
+{
+    CGLContextObj cgl_ctx = [self willStartDrawing];
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     [[self openGLContext] flushBuffer];
+
+    [self didFinishDrawing:cgl_ctx];
+
+    return kCVReturnSuccess;
+}
+
+- (void) reshape
+{
+    CGLContextObj cgl_ctx = [self willStartDrawing];
+	[super reshape];
+    [self didFinishDrawing:cgl_ctx];
+}
+
+- (void) update
+{
+    CGLContextObj cgl_ctx = [self willStartDrawing];
+	[super update];
+    [self didFinishDrawing:cgl_ctx];
+}
+
+- (void)dealloc
+{
+    CVDisplayLinkRelease(displayLink);
+    [super dealloc];
+}
+
+- (void) drawRect:(NSRect)dirtyRect
+{
+    [self render];
+}
+
+static CVReturn displayLinkCallback(CVDisplayLinkRef displayLink,
+  const CVTimeStamp* now, const CVTimeStamp* outputTime, CVOptionFlags flagsIn,
+  CVOptionFlags* flagsOut, void* displayLinkContext)
+{
+    [(OpenGLView*)displayLinkContext render];
+    return kCVReturnSuccess;
 }
 
 @end
